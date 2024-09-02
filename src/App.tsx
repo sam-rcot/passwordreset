c320f9524c9cc1c13d57240d06629f9e3bd7a4c1
samliburd <liburdsam@gmail.com>
2024-09-02 18:21:19 +0100

    fixed width

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,57 @@
 import './App.css';
-import { FormEvent, useState } from "react";
-import {
-    HashRouter as Router,
-    Routes,
-    Route
-} from "react-router-dom";
-import NavBar from "./components/navbar/NavBar.tsx";
-import General from "./components/general/General.tsx";
-import Stakeholder from "./components/stakeholder/Stakeholder.tsx";
+import { FormEvent, useState } from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/navbar/NavBar.tsx';
+import General from './components/general/General.tsx';
+import Stakeholder from './components/stakeholder/Stakeholder.tsx';
 
 function App() {
-    const handleSubmit = (event: FormEvent) => {
-        event.preventDefault();
-    };
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+  };
 
-    const [formValues, setFormValues] = useState<{ [key: string]: string }>({});
+  const [formValues, setFormValues] = useState<{ [key: string]: string }>({});
 
-    const handleInputChange = (field: string, value: string) => {
-        setFormValues((prevValues) => ({
-            ...prevValues,
-            [field]: value
-        }));
-    };
+  const handleInputChange = (field: string, value: string) => {
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [field]: value,
+    }));
+  };
 
-    return (
-        <>
-            <Router>
-                <nav className="flex justify-center"><NavBar/></nav>
-                <main className="h-screen w-screen bg-white text-slate-900 flex justify-center items-center flex-col">
-                    <Routes>
-                        <Route
-                            path="/"
-                            element={<General
-                                handleSubmit={handleSubmit}
-                                handleInputChange={handleInputChange}
-                                formValues={formValues}
-                            />}
-                        />
-                        <Route
-                            path="/stakeholder"
-                            element={<Stakeholder
-                                handleSubmit={handleSubmit}
-                                handleInputChange={handleInputChange}
-                                formValues={formValues}
-                            />}
-                        />
-                    </Routes>
-                </main>
-            </Router>
-        </>
-    );
+  return (
+    <>
+      <Router>
+        <nav className="flex justify-center">
+          <NavBar />
+        </nav>
+        <main className="flex min-h-screen w-full flex-col items-center justify-start bg-white pt-10 text-slate-900">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <General
+                  handleSubmit={handleSubmit}
+                  handleInputChange={handleInputChange}
+                  formValues={formValues}
+                />
+              }
+            />
+            <Route
+              path="/stakeholder"
+              element={
+                <Stakeholder
+                  handleSubmit={handleSubmit}
+                  handleInputChange={handleInputChange}
+                  formValues={formValues}
+                />
+              }
+            />
+          </Routes>
+        </main>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,15 +1,14 @@
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-    return (
-        <>
-            <div className="flex gap-3 items-center justify-center
-            text-2xl font-bold py-3 font-nunito underline text-dark-teal">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/stakeholder">Stakeholder</NavLink>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="box-border flex items-center justify-center gap-3 py-3 font-nunito text-2xl font-bold text-dark-teal underline">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/stakeholder">Stakeholder</NavLink>
+      </div>
+    </>
+  );
+};
 
 export default NavBar;
