88dd61caf26194063f2e0bb8a482d1578efb387b
Sam Liburd <sam.liburd@rcot.co.uk>
2024-09-04 10:10:24 +0100

    updated dataLayer push

--- src/components/checkbox/CheckBox.tsx
+++ src/components/checkbox/CheckBox.tsx
@@ -17,21 +17,22 @@ const CheckBox = ({
                     ...props
                   }: CheckBoxProps) => {
   const [checked, setChecked] = React.useState(true);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setChecked(e.target.checked);
     onChange(e.target.checked);
-  };
-  const handleClick = () => {
+
+    // Fire dataLayer push only when the state changes
     window.dataLayer.push({
       event: "checkbox_click",
       clickText: labelText,
+      checked: e.target.checked, // Optionally pass whether it was checked or not
     });
   };
 
   return (
     <div
       className={`relative flex h-fit w-full items-center justify-between gap-3 rounded-lg border p-4 font-nunito text-dark-teal transition-transform duration-200 group-hover:scale-110 ${className}`}
-      onClick={handleClick}
     >
       <input
         className="peer h-6 w-6 rounded-sm border-gray-300 text-dark-teal hover:border-teal/50 focus:ring-teal/50"
@@ -40,14 +41,14 @@ const CheckBox = ({
         type={type}
         value={id}
         checked={checked}
-        onChange={handleChange}
+        onChange={handleChange} // Handle change event, includes dataLayer push
       />
       <label
         className="flex h-6 w-fit cursor-pointer flex-row items-center justify-items-start"
         htmlFor={id}
         {...props} // This passes the data-button-id to the label
       >
-        <p className="">{labelText}</p>
+        <p>{labelText}</p>
       </label>
     </div>
   );
