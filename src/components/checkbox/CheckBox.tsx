cc241d25f83cbac03ab84891605e26e6b2aadea0
Sam Liburd <sam.liburd@rcot.co.uk>
2024-09-04 09:02:33 +0100

    added dataLayer push

--- src/components/checkbox/CheckBox.tsx
+++ src/components/checkbox/CheckBox.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 type CheckBoxProps = {
   labelText: string;
@@ -9,22 +9,28 @@ type CheckBoxProps = {
 };
 
 const CheckBox = ({
-  labelText,
-  type = 'checkbox',
-  className,
-  id,
-  onChange,
-  ...props
-}: CheckBoxProps) => {
+                    labelText,
+                    type = "checkbox",
+                    className,
+                    id,
+                    onChange,
+                    ...props
+                  }: CheckBoxProps) => {
   const [checked, setChecked] = React.useState(true);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setChecked(e.target.checked);
     onChange(e.target.checked);
   };
+  const handleClick = () => {
+    window.dataLayer.push({
+      event: "checkbox_click"
+    });
+  };
 
   return (
     <div
       className={`relative flex h-fit w-full items-center justify-between gap-3 rounded-lg border p-4 font-nunito text-dark-teal transition-transform duration-200 group-hover:scale-110 ${className}`}
+      onClick={handleClick}
     >
       <input
         className="peer h-6 w-6 rounded-sm border-gray-300 text-dark-teal hover:border-teal/50 focus:ring-teal/50"
