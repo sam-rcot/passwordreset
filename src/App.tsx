bfc3f3ec5e5bb35fa99ea722a03f6154a1160059
Sam Liburd <sam.liburd@rcot.co.uk>
2024-10-02 14:44:38 +0100

    added cookie consent

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
-import { FormEvent, useState } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/navbar/NavBar.tsx';
-import General from './components/general/General.tsx';
-import Stakeholder from './components/stakeholder/Stakeholder.tsx';
+import "./App.css";
+import { FormEvent, useState, useEffect } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/navbar/NavBar";
+import General from "./components/general/General";
+import Stakeholder from "./components/stakeholder/Stakeholder";
+import CookieConsent, { getCookieConsentValue } from "react-cookie-consent";
 
 function App() {
   const handleSubmit = (event: FormEvent) => {
@@ -15,9 +16,17 @@ function App() {
   const handleInputChange = (field: string, value: string) => {
     setFormValues((prevValues) => ({
       ...prevValues,
-      [field]: value,
+      [field]: value
     }));
   };
+
+  useEffect(() => {
+    // Handle GTM event push based on cookie consent
+    if (getCookieConsentValue() === "true") {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({ event: "cookieConsentGranted" });
+    }
+  }, []);
 
   return (
     <>
@@ -52,6 +61,38 @@ function App() {
           </main>
         </div>
       </Router>
+
+      {/* Cookie Consent Banner */}
+      <CookieConsent
+        location="bottom"
+        buttonText="I understand"
+        declineButtonText="Decline"
+        enableDeclineButton
+        onAccept={() => {
+          window.dataLayer = window.dataLayer || [];
+          // Update the consent status to allow ad storage and analytics storage
+          window.dataLayer.push({
+            event: "consentUpdate",
+            ad_storage: "granted",
+            analytics_storage: "granted"
+          });
+        }}
+        onDecline={() => {
+          window.dataLayer = window.dataLayer || [];
+          // Update the consent status to deny ad storage and analytics storage
+          window.dataLayer.push({
+            event: "consentUpdate",
+            ad_storage: "denied",
+            analytics_storage: "denied"
+          });
+        }}
+        style={{ background: "#003643", fontFamily: "Nunito Sans", color: "white" }}
+        buttonStyle={{ backgroundColor: "#43cfb5", fontFamily: "Nunito Sans", color: "white" }}
+        declineButtonStyle={{ backgroundColor: "#e8705e", fontFamily: "Nunito Sans" }}
+        expires={150}
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
     </>
   );
 }
